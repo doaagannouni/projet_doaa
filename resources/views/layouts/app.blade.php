fb4342480ec68c17405680760a4ac9550d7b9b18
Gannouni Doua <150908416+doaagannouni@users.noreply.github.com>
2023-12-16 13:22:23 +0100

    oot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,25 +1,49 @@
-<!-- resources/views/layouts/app.blade.php -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple CRUD Website by Doua Gannouni for Azure</title>
-    
-    <!-- Include Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Include your other CSS files here -->
+    <title>Laravel Quickstart - Basic</title>
+
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+    <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
 </head>
-<body>
-    <div class="container-fluid bg-primary text-white p-3 text-center">
-        <h1>Simple CRUD Website by Doua Gannouni for Azure</h1>
-    </div>
+<body id="app-layout">
+    <nav class="navbar navbar-default">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Task List
+                </a>
+            </div>
+
+        </div>
+    </nav>
 
     @yield('content')
 
-    <!-- Include Bootstrap JS and other JS files here -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\ProductController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +19,6 @@ use Illuminate\Http\Request;
 /**
     * Show Task Dashboard
     */
-/* 
 Route::get('/', function () {
     Log::info("Get /");
     $startTime = microtime(true);
@@ -36,6 +33,9 @@ Route::get('/', function () {
     }
 });
 
+/**
+    * Add New Task
+    */
 Route::post('/task', function (Request $request) {
     Log::info("Post /task");
     $validator = Validator::make($request->all(), [
@@ -60,7 +60,7 @@ Route::post('/task', function (Request $request) {
 
 /**
     * Delete Task
-   
+    */
 Route::delete('/task/{id}', function ($id) {
     Log::info('Delete /task/'.$id);
     Task::findOrFail($id)->delete();
@@ -68,47 +68,4 @@ Route::delete('/task/{id}', function ($id) {
     Cache::flush();
 
     return redirect('/');
-});
-
-*/
-
-use App\Models\Product;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Validator;
-
-// Afficher tous les produits
-Route::get('/products', function () {
-    $products = Product::all();
-    return view('products.index', compact('products'));
-});
-
-// Afficher le formulaire de création de produit
-Route::get('/products/create', function () {
-    return view('products.create');
-});
-
-// Stocker un nouveau produit
-Route::post('/products', function (Request $request) {
-    Product::create($request->all());
-    return redirect('/products');
-});
-
-// Afficher le formulaire de modification de produit
-Route::get('/products/{product}', function (Product $product) {
-    return view('products.edit', compact('product'));
-});
-
-// Mettre à jour un produit existant
-Route::put('/products/{product}', function (Request $request, Product $product) {
-    $product->update($request->all());
-    return redirect('/products');
-});
-
-// Supprimer un produit
-Route::delete('/products/{product}', function (Product $product) {
-    $product->delete();
-    return redirect('/products');
 });
